4b56f88f0e0495222a7e31eee157710eea17a0f2
spiano <lafleurpiano@gmail.com>
2020-09-17 20:38:37 -0500

    built out basic gif application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from 'axios'
+
+const BASEURL = "https://api.giphy.com/v1/gifs/search?q=";
+const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
 
 function App() {
+    const [results, setResults] = useState([]);
+    const [query, setQuery] = useState('kittens')
+
+// use effect takes in a function
+    useEffect(() => {
+        searchGify(query)
+    }, [query]); // an empty array will ask this to run only once
+    // if you ever want to rerun your method, you set the query inside the array
+// you cannot put asynchronous code inside the use effect function
+
+    const searchGify = query => {
+        console.log('Searching Giffy...');
+        axios(`${BASEURL}${query}${APIKEY}`)
+            .then(response => setResults(response.data.data))
+            .catch(err=> console.log(err))
+    };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input onChange={e => setQuery(e.target.value)} />
+        <br/>
+      {results.map(result => <img src={result.images.original.url} key={result.id}/>)}
     </div>
   );
 }
